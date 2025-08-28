e755235f8e557f9c2f417b4a5d73abc29c53bc14
carlaldos <carlaldosvargaspausto@gmail.com>
2025-08-28 11:44:06 +0800

    firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCI3NEM700cDTXY1qEEBfB5UqXW82gcNyU",
+  authDomain: "cvp-goals-tracker.firebaseapp.com",
+  projectId: "cvp-goals-tracker",
+  storageBucket: "cvp-goals-tracker.firebasestorage.app",
+  messagingSenderId: "1002144500710",
+  appId: "1:1002144500710:web:ab24e9ea4753d1cb2a2427"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
